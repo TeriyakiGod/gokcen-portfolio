9d7abfc43e3e77b6a93856116d3e7e79410e8ef1
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2024-03-25 00:05:46 +0100

    project init

---
+++ content/gallery.md
@@ -0,0 +1,5 @@
+Title: Art Gallery
+Date: 24/04/2024
+Category: Gallery
+gallery: {photo}art{Art}
+template: gallery
---
+++ pelicanconf.py
@@ -0,0 +1,48 @@
+AUTHOR = 'Kacper Akdag-Ochnik'
+SITENAME = 'Gokcen Akdag-Ochnik'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+PLUGINS = ['photos']
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+PHOTO_LIBRARY = "./content/images"
+PHOTO_GALLERY = (1024, 768, 80)
+PHOTO_ARTICLE = (760, 506, 80)
+PHOTO_THUMB = (192, 144, 60)
+PHOTO_SQUARE_THUMB = False
+PHOTO_RESIZE_JOBS = 0
+PHOTO_WATERMARK = True
+PHOTO_WATERMARK_TEXT = SITENAME
+
+THEME = "notmyidea"
